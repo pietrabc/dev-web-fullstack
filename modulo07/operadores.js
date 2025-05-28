670a536b451c2daa16cda4006f47e26763637c3d
pietrabc <pietrabezerracardoso@gmail.com>
2025-05-28 18:26:54 -0300

    hashtag-js

---
+++ modulo07/operadores.js
@@ -0,0 +1,20 @@
+// Adição + Subtração - Multiplicação * Divisão / Módulo % Inversão de sinal/negativo -
+
+// Operadores aritméticos avançados
+// Exponenciação **
+let base = 2;
+let expoente = 3;
+let resultado = base ** expoente; // 2 seja elevado a 3  2 * 2 * 2
+console.log(resultado);
+
+// Incremento ++ (adicionar o valor 1) Decremento -- ( diminuindo o valor 1)
+let controle = 0;
+console.log(--controle);
+console.log(--controle);
+console.log(--controle);
+console.log(--controle);
+// console.log(++controle);
+// console.log(controle);
+
+// console.log(controle--);
+// console.log(controle);
