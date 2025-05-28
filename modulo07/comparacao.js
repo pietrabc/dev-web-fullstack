a01c49adf238890f226fb0ed43aa5a8e0a7846cc
pietrabc <pietrabezerracardoso@gmail.com>
2025-05-28 18:51:11 -0300

    hashtag-js

---
+++ modulo07/comparacao.js
@@ -0,0 +1,18 @@
+// Igualdade e Estrita Igualdade
+console.log(5 == '5');
+console.log(5 === '5'); // Boa prática
+console.log(5 + 5);
+
+console.log(5 !== '5'); // Boa prática
+
+// Maior que e Menor que
+console.log(10 > 20); // false
+console.log(10 >= 10);
+
+console.log(10 <= 20);
+
+
+let senha = 1234;
+let senhaDigitada = 12345;
+let comparacao = senha === senhaDigitada;
+console.log("Aqui é a minha senha:" + comparacao)
