f9178d813d3660aa59ba70117e5cefd20542707f
pietrabc <pietrabezerracardoso@gmail.com>
2025-05-28 18:14:43 -0300

    hahstag-js

---
+++ modulo07/aritmetico.js
@@ -0,0 +1,21 @@
+// Adição
+
+let valor = 10;
+let segundoValor = 43;
+
+console.log(valor + segundoValor);
+
+// Subtração
+console.log(valor - segundoValor);
+console.log(segundoValor - valor);
+
+//Multiplicação
+console.log(valor * segundoValor);
+
+// Divisão
+console.log(valor / segundoValor);
+console.log(segundoValor / valor);
+
+// Módulo ( resto da divisão inteira)
+console.log(segundoValor % valor);
+console.log(valor % segundoValor);
