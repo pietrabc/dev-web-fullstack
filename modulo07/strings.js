d4c8e6b4e3ffc1df4175047e1f892505846ddab4
pietrabc <pietrabezerracardoso@gmail.com>
2025-05-28 16:42:18 -0300

    hashttag-js

---
+++ modulo07/strings.js
@@ -0,0 +1,26 @@
+// String 
+
+// "Um texto" - string
+// 'Criar mensagem' - string
+// `Criar mais um texto` - string template
+
+let mensagem = "Olá, ";
+let nome = 'Paulo';
+let mensagem2 = `Olá, ${nome}. Bem vindo ao curso`;
+
+console.log(mensagem + nome + "Bem vindo");
+console.log(mensagem2)
+
+// indice = 0
+console.log(nome[3] = "Q");
+
+// Number
+let numero = 30;
+let numero1 = -56;
+let decimal = 3.14;
+console.log(numero, numero1, decimal)
+// Infinity e NaN 
+
+let string = "10";
+let dez = 10;
+console.log(`${string} e ${dez}`)
