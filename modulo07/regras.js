36207a1ba67df55bfdb9f0bed98a77207da276bf
pietrabc <pietrabezerracardoso@gmail.com>
2025-05-28 14:27:25 -0300

    hashtag-js
    Treino de Regras

---
+++ modulo07/regras.js
@@ -0,0 +1,20 @@
+let nomeProduto = "Boné";
+let precoProduto = 50;
+let quantidade = 3;
+
+let minhaMensagem = "Hello, World!";
+// const const = " Isso não é uma boa prática";
+
+let _email = "email@email.com";
+let funcionario_email = "email@outroemail.com";
+
+let mensagem = "Hello!";
+
+let segundaMensagem = 'Hello, again!'
+
+let texto = "Quero dar ênfase na próxima palavra: 'Essa";
+let texto2 = "Quero dar ênfase na próxima palavra: 'Essa' ";
+
+const PI = 3.14;
+const MAX_WIDTH = 769;
+
