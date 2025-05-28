05c58b2d8ecd2b484bc964c0d5230558d9ff3008
pietrabc <pietrabezerracardoso@gmail.com>
2025-05-28 19:02:03 -0300

    hashtag-js

---
+++ modulo07/coercao.js
@@ -0,0 +1,8 @@
+// Coerção Implicita
+console.log(5 == '5'); // igualdade - coerção implícita
+console.log(5 === '5');
+console.log(5 + '5'); // Número é convertido para string
+console.log('10' - 5); // string convertida para number
+console.log('3' * '2'); // strings convertidas para number
+
+console.log(null == undefined);
