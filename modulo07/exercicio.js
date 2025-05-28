056786c863f67c94e5fb00a4e06a3539c5d490e6
pietrabc <pietrabezerracardoso@gmail.com>
2025-05-28 17:50:17 -0300

    hashtag-js

---
+++ modulo07/exercicio.js
@@ -0,0 +1,40 @@
+// 1) Escreva um programa simples que exiba uma string no terminal utilizando o console.log
+
+console.log('Hello, world');
+
+// 2) Modifique o programa anterior para armazenar a mensagem em uma variável antes de exibi-la no console
+let nome1 = 'Pietra';
+console.log(nome1);
+
+// 3) Declare três variáveis mutáveis: nome(string), idade(number) e isStudent(boolean)
+let nome = 'Bezerra';
+let idade = 23;
+let isStudent = true;
+
+// 4) Modifique o valor das variáveis anteriores e imprima os novos valores no console
+
+nome = 'Cardoso';
+idade = 24;
+isStudent = false;
+console.log(nome, idade, isStudent);
+
+// 5) Crie uma variável constante que irá armazenar um número e imprima no console;
+const numero = 45;
+console.log(numero);
+
+// 6) Declare duas variáveis, endereco e telefone, sem atribuir valores a elas
+let endereco;
+let telefone;
+
+// 7) Declare variáveis com nomes descritivos para armazenar o nome de um produto, seu preço e quantidade em estoque.
+let nomeProduto = 'Notebook';
+let precoProduto = 1500;
+let quantidadeProduto = 20;
+
+console.log(nomeProduto, precoProduto, quantidadeProduto);
+
+// 8) Declare uma variável para armazenar o nome de uma cidade e outra para armazenar a sua população. Exiba a mensagem combinando ambas as variáveis
+let cidade = 'Barbalha';
+let populacao = 20000;
+
+console.log(`A cidade de ${cidade} tem uma população de ${populacao} habitantes.`)
