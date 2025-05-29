527e1821cad8abe002d5174891b40f763fa40daa
pietrabc <pietrabezerracardoso@gmail.com>
2025-05-28 22:27:40 -0300

    hashtag-js

---
+++ modulo07/explicita.js
@@ -0,0 +1,21 @@
+// Converção Explicita
+let numero = 123;
+console.log(typeof numero);
+numero = String(numero)
+console.log(typeof numero);
+
+let numero2 = 45;
+console.log(numero2);
+console.log(numero2.toString());
+
+let string = "Meu texto";
+let num = 0;
+console.log(Boolean(string));
+console.log(Boolean(num));
+
+let stringNumero = '12345';
+console.log(Number(stringNumero));
+console.log(Number(string));
+
+let booleano = true;
+console.log(String(booleano));
