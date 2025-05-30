d167961824324f854a6195aa7dbf55d11c69f49a
pietrabc <pietrabezerracardoso@gmail.com>
2025-05-30 17:07:40 -0300

    hashtag-js

---
+++ modulo07/array.js
@@ -0,0 +1,17 @@
+// Array
+
+let lista = ['Banana', 23, true, 'Maçã']; //indice 0 
+
+lista = ['banana', 'maçã', 'pêra'];
+
+console.log(lista);
+
+lista[0] = 'laranja';
+console.log(lista[0]);
+
+console.log(lista);
+
+let nome = 'João';
+nome[0] = 'M';
+
+console.log(nome);
