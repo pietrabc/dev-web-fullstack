8c4dfd2a7f6ba237de2bc0863aafcde1d067930f
pietrabc <pietrabezerracardoso@gmail.com>
2025-06-02 16:37:02 -0300

    hashtag-js

---
+++ modulo08/funcao05.js
@@ -0,0 +1,19 @@
+// Declaração e Expressão de Função
+
+// function soma(a, b) {
+//   return a + b;
+// }
+
+// let valor = soma(2, 4);
+// console.log(valor);
+
+// let total = soma(3, 4) + 10;
+// console.log(total);
+
+let total = function (a, b) {
+  return a + b;
+};
+
+console.log(total(3, 4));
+console.log(total(10, 4));
+console.log(total(45, 4));
