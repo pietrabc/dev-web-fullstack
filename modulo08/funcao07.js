610b2f8fc488a2a3479bcc72b63a8483896c9eed
pietrabc <pietrabezerracardoso@gmail.com>
2025-06-02 18:28:28 -0300

    hashtag-js

---
+++ modulo08/funcao07.js
@@ -0,0 +1,7 @@
+// Parâmetros Opcionais e Valores padrão
+
+function cumprimentar(nome = 'Visitante', saudacao = 'Boa tarde') {
+    console.log(`Olá ${nome}, ${saudacao}`);
+}
+
+cumprimentar('Pietra' );
