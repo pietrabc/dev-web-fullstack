62120688e32262c1567e8e7d38989dfaa184ad23
pietrabc <pietrabezerracardoso@gmail.com>
2025-06-02 14:01:33 -0300

    hashtag-js

--- modulo08/funcao02.js
+++ modulo08/funcao02.js
@@ -1,5 +1,16 @@
 // Chamadas de funçôes
 
-function exibirDetalhes(nome, curso) [
-    console.log('aaa')
-]
+function exibirDetalhes(nome, curso) {
+  console.log('Professor:' + nome +  ' Curso: ' + curso);
+}
+
+exibirDetalhes('Carlos', 'Geografia');
+exibirDetalhes('Renata', 'Português');
+exibirDetalhes('Luiz', 'Matemática');
+exibirDetalhes('Ricardo', 'Física');
+
+function mensagem() {
+    console.log('Imprimindo uma mensagem!');
+}
+
+mensagem();
