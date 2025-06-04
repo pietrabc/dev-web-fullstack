f3af9f8e382e65fff055aa53850496da70a64045
pietrabc <pietrabezerracardoso@gmail.com>
2025-06-04 19:31:49 -0300

    hashtag-js

---
+++ modulo09/metodos07.js
@@ -0,0 +1,7 @@
+let frutas = ['maçã', 'uva', 'laranja'];
+
+console.log(frutas.indexOf('laranja'));
+
+console.log(frutas.splice(0, 1)); // início, quantidade de elementos
+console.log(frutas.splice(2, 1, 10)); // substituir um elemento - início, quantidade, valor substituindo
+console.log(frutas);
