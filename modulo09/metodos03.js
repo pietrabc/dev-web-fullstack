139056198af0a0ec24c17a0e8dfc9ca27debf579
pietrabc <pietrabezerracardoso@gmail.com>
2025-06-04 16:24:29 -0300

    hashtag-js

---
+++ modulo09/metodos03.js
@@ -0,0 +1,8 @@
+//String - Sequência de caracteres - Parte02
+let string = 'Olá, Mundo!';
+
+// replace(search, replaceWith);
+console.log(string.replace('Mundo', 'JavaScript')); // não substitui o valor original
+
+// indexOf()
+console.log(string.indexOf('Mundo'));
