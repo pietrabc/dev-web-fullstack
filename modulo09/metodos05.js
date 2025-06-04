bfb249b9a254b6f26beba013e7ea33a0a98a16de
pietrabc <pietrabezerracardoso@gmail.com>
2025-06-04 18:00:51 -0300

    hashtag-js

---
+++ modulo09/metodos05.js
@@ -0,0 +1,14 @@
+let produtos = ['Carrinho', 'Boneca', 'Bola'];
+// .push() ao final da lista
+produtos.push('Navio', 'Corda');
+console.log(produtos);
+// .unshift() no início da lista
+produtos.unshift('Video Game');
+console.log(produtos);
+
+// .pop remover o último elemento
+produtos.pop();
+console.log(produtos);
+// .shift() remove o primeiro elemento
+produtos.shift();
+console.log(produtos);
