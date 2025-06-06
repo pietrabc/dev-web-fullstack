974039411d4a4b0654f3baac64899f0b89b0da2d
pietrabc <pietrabezerracardoso@gmail.com>
2025-06-05 21:08:25 -0300

    hashtag-js

---
+++ modulo09/metodos09.js
@@ -0,0 +1,12 @@
+const objeto = {
+  propriedade: 10,
+  metodos: function () {
+    console.log('Olá, eu sou um método');
+  },
+};
+
+const string = 'Eu sou louco por JavaScript';
+console.log(string.length);
+
+const array = [1, 30, 24, 55];
+console.log(array.length);
