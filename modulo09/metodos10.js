6d6351c8f3e9846fc0224bfe4f77d5fe69b0222b
pietrabc <pietrabezerracardoso@gmail.com>
2025-06-05 21:21:04 -0300

    hashtag-js

---
+++ modulo09/metodos10.js
@@ -0,0 +1,11 @@
+// Métodos Nativos dos Objetos pt 1
+
+const produto = {
+  nome: 'Laptop',
+  preco: 2500,
+  disponibilidade: true,
+};
+
+console.log(Object.keys(produto)); // chave em uma lista
+console.log(Object.values(produto)); // valores em uma lista
+console.log(Object.entries(produto)); // lista contendo listas de chave e valor
