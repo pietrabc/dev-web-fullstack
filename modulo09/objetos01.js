a4e9bc0a24b7d898f68ed2f69d2a92a4c4bb2564
pietrabc <pietrabezerracardoso@gmail.com>
2025-07-01 15:22:50 -0300

    hashtag-js

---
+++ modulo09/objetos01.js
@@ -0,0 +1,11 @@
+// Math = operações matemáticas e Date = datas
+// PI = 3.14159...
+
+const PI = Math.PI;
+console.log(PI);
+
+const raizQuadrada = Math.sqrt(16);
+console.log(raizQuadrada);
+
+const potencia = Math.pow(2, 8);
+console.log(potencia);
---
+++ modulo09/objetos02.js
@@ -0,0 +1,3 @@
+// Math.round(x) - Arredonda x para o inteiro mais próximo
+
+// Math.random() - Retorna um número pseudo-aleatório entre 0 e 1
