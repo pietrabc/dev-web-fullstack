008a58d8a882c1a767b11698b579f493e880cd03
pietrabc <pietrabezerracardoso@gmail.com>
2025-07-01 15:37:50 -0300

    hashtag-js

--- modulo09/objetos02.js
+++ modulo09/objetos02.js
@@ -1,3 +1,8 @@
 // Math.round(x) - Arredonda x para o inteiro mais próximo
+let numero = 3.45;
+let numero2 = 3.55;
 
+console.log(Math.round(numero2));
 // Math.random() - Retorna um número pseudo-aleatório entre 0 e 1
+const aleatorio = Math.random()*100;
+console.log(parseFloat(aleatorio.toFixed(2)));
