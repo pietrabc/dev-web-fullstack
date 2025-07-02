3a58987984e5956d7068b5b7b2dd484a3c15a92c
pietrabc <pietrabezerracardoso@gmail.com>
2025-07-02 11:50:56 -0300

    hashtag-js

---
+++ modulo10/aula01.js
@@ -0,0 +1,11 @@
+const mensagemDeBoasVindas1 = 'Bem vindo, torcedor Tricolor!';
+const mensagemDeBoasVindas2 = 'Bem vindo, torcedor Alvinegro!';
+
+console.log(mensagemDeBoasVindas1);
+console.log(mensagemDeBoasVindas2);
+
+const mensagemDeEscolha = 'Clique no setor para qual deseja comprar seu ingresso!';
+const mensagemFinal = 'Divirta-se no Castelão!!!'
+
+console.log(mensagemDeEscolha);
+console.log(mensagemFinal);
