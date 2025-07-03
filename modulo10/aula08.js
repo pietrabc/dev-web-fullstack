9d72af6db55865da14a7bb284e6059360d99943c
pietrabc <pietrabezerracardoso@gmail.com>
2025-07-03 13:57:22 -0300

    hashtag-js

---
+++ modulo10/aula08.js
@@ -0,0 +1,19 @@
+// While
+
+let numero = 1;
+
+while (numero <= 5) {
+    console.log('Número: ' + numero);
+    numero++; // numero + 1
+}
+
+console.log('Contagem com While finalizada');
+
+let funcionariosRegistrados = 0;
+
+while (funcionariosRegistrados < 3) {
+    console.log('Funcionário ' + (funcionariosRegistrados + 1) + ' Registrou suas horas.')
+    funcionariosRegistrados++;
+}
+
+console.log('Fim de registro');
