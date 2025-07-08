0606f72571fbb3020a4925fa321a4792d30ccae8
pietrabc <pietrabezerracardoso@gmail.com>
2025-07-08 20:08:10 -0300

    hashtag-js

---
+++ modulo10/aula15.js
@@ -0,0 +1,27 @@
+const namesObject = {
+  0: 'Daniel',
+  1: 'Izabele',
+  2: 'Fabrício',
+  3: 'Rodrigo',
+  4: 'Jorge',
+  5: 'Lira',
+  6: 'Camila',
+  7: 'Matheus',
+  8: 'Gabriel',
+};
+
+const names = [
+  'Daniel',
+  'Izabele',
+  'Fabrício',
+  'Rodrigo',
+  'Jorge',
+  'Lira',
+  'Camila',
+  'Matheus',
+  'Gabriel',
+];
+
+for (personName of names) {
+    console.log(personName);
+}
