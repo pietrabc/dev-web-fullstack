3f47b40368113ae7650724aee19da3c3e5534db9
pietrabc <pietrabezerracardoso@gmail.com>
2025-07-19 18:30:20 -0300

    hashtag-js

---
+++ modulo10/exercicio05.js
@@ -0,0 +1,8 @@
+// Exercício 1: Cálculo do Faturamento Semanal
+// Você é o gerente de uma lanchonete e deseja calcular o valor total das vendas de hambúrgueres em uma
+// semana. Cada hambúrguer custa R$10, e você registra o número de hambúrgueres vendidos a cada dia da
+// semana. Crie um programa que calcule o valor total das vendas em uma semana.
+
+// Exercício 2: Listagem do Cardápio Digital
+// Você deseja criar um cardápio digital para sua lanchonete. Crie um programa que liste os itens do cardápio
+// juntamente com seus preços. Utilize um loop for para percorrer o cardápio e exibi-lo.
