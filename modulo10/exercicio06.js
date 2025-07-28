fd97b0290a5cba298b0d61dc46dc1ff47c1e6429
pietrabc <pietrabezerracardoso@gmail.com>
2025-07-28 16:20:52 -0300

    hashtag-js

--- modulo10/exercicio06.js
+++ modulo10/exercicio06.js
@@ -3,7 +3,7 @@
 // feita. Crie um programa que simule a contagem do dinheiro recebido a cada venda até o fechamento da
 // lanchonete.
 
-
+//while (condição) {bloco de instrução}
 // Exercício 4: Entrega de Pedidos Pendentes
 // Você é o entregador de uma lanchonete e precisa entregar pedidos até que não haja mais pedidos
 // pendentes. Crie um programa que simule a entrega de pedidos até que não haja mais pedidos na lista.
