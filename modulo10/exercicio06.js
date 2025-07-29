665e19bf359991991805d942a4d5d6bcf2d8647a
pietrabc <pietrabezerracardoso@gmail.com>
2025-07-29 13:33:17 -0300

    hashtag-js

--- modulo10/exercicio06.js
+++ modulo10/exercicio06.js
@@ -4,6 +4,51 @@
 // lanchonete.
 
 //while (condição) {bloco de instrução}
+
+function realizarVendas(totalDeVendas) {
+  let dinheiroRecebido = 0;
+  let vendasRealizadas = 0;
+  let vendasConcluidas = false;
+
+  while (!vendasConcluidas) {
+    const valorDaVenda = 20;
+
+    //Simular venda
+    dinheiroRecebido += valorDaVenda;
+    vendasRealizadas++;
+
+    //Verificar as vendas atingiramo o número
+    if (vendasRealizadas >= totalDeVendas) {
+      vendasConcluidas = true;
+    }
+  }
+  console.log(`O total de dinheiro recebido: R$${dinheiroRecebido.toFixed(2)}`);
+}
+
+const totalDeVendasHoje = 42;
+realizarVendas(totalDeVendasHoje);
+
 // Exercício 4: Entrega de Pedidos Pendentes
 // Você é o entregador de uma lanchonete e precisa entregar pedidos até que não haja mais pedidos
 // pendentes. Crie um programa que simule a entrega de pedidos até que não haja mais pedidos na lista.
+
+const pedidosPendentes = [
+  'Hamburguer',
+  'Batata frita',
+  'Refrigerante',
+  'Milshake',
+  'Nuggets',
+]; // iindice 0
+
+function realizarEntregas(pedidosPendentes) {
+  let pedidoAtual = "";
+  console.log('Iniciando Entregas:');
+
+  while (pedidosPendentes.length > 0) {
+    pedidoAtual = pedidosPendentes.shift();
+    console.log(`Entregando: ${pedidoAtual}`);
+  }
+  console.log('Todos os pedidos foram entregues!');
+}
+
+realizarEntregas(pedidosPendentes);
