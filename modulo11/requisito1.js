92c86a64672b93c5b89a2b0874256c04e07465ea
pietrabc <pietrabezerracardoso@gmail.com>
2025-08-18 18:57:14 -0300

    exercicio-js
    Início do Módulo 11, exercícios para prosseguir no curso

---
+++ modulo11/requisito1.js
@@ -0,0 +1,14 @@
+// Requisito 1: : Crie uma função que receba dois números inteiros e retorne a soma deles.
+// Função: somar
+// Exemplo Entrada: // somar(5, 3);
+// Exemplo Saída: // 8
+
+let num = 5;
+let numero = 3
+
+function somarNumeros(num,numero) {
+    let resultado = num + numero;
+    console.log(resultado);
+}
+
+somarNumeros(num, numero);
