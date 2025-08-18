b461997746812bf8506f73dc84502e0252ad3201
pietrabc <pietrabezerracardoso@gmail.com>
2025-08-18 19:44:40 -0300

    hashtag-js

---
+++ modulo11/requisito2.js
@@ -0,0 +1,4 @@
+// Requisito 2: Crie uma função que receba um array de nomes e retorne esse array ordenado
+// em ordem alfabética. Função: ordenarNomes
+// Exemplo Entrada: // ordenarNomes(['Ana', 'Elias', 'Carlos', 'Beatriz']);
+// Exemplo Saída: // ['Ana', 'Beatriz', 'Carlos', ‘Elias’]
