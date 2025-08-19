4864a2d2d4c2c0c7a965fcfb4b1188b66de87d4f
pietrabc <pietrabezerracardoso@gmail.com>
2025-08-19 17:48:34 -0300

    hashtag-js

--- modulo11/requisito2.js
+++ modulo11/requisito2.js
@@ -2,3 +2,10 @@
 // em ordem alfabética. Função: ordenarNomes
 // Exemplo Entrada: // ordenarNomes(['Ana', 'Elias', 'Carlos', 'Beatriz']);
 // Exemplo Saída: // ['Ana', 'Beatriz', 'Carlos', ‘Elias’]
+
+function ordenarNomes(listaNomes) {
+    return listaNomes.sort();
+}
+
+let nomes = ['Ana', 'Elisa', 'Amanda', 'Gabriel','Zeus', 'Beatriz', 'Pietra'];
+console.log(ordenarNomes(nomes))
---
+++ modulo11/requisito3.js
@@ -0,0 +1,8 @@
+// Requisito 3: Crie uma função que receba dois valores e retorne verdadeiro se ambos forem
+// considerados "truthy".
+// Função: compararValores
+// Exemplo Entrada: // compararValores(5, "texto");
+// // compararValores(0, "texto");
+// Exemplo Saída: // true
+// // false
+
