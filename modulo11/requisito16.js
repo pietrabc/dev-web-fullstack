c4ef4141d407204be5c9b769c391afd1bd0dc5c6
pietrabc <pietrabezerracardoso@gmail.com>
2025-08-20 18:22:18 -0300

    hashtag-js

---
+++ modulo11/requisito16.js
@@ -0,0 +1,10 @@
+// Exercício 16: Crie uma função que receba uma frase e codifique as vogais de acordo com a
+// seguinte substituição: a → 1, e → 2, i → 3, o → 4, u → 5. Crie uma segunda função que
+// decodifique a frase, revertendo as substituições.
+// Funções: codificar e decodificar
+// Exemplo Entrada: // const frase = "a casa e o sol";
+// const resultado = codificar(frase);
+// const frase2 = "1 c1s2 2 4 s4l";
+// const resultado2 = decodificar(frase2);
+// Exemplo Saída: // "1 c1s2 2 4 s4l"
+// // "a casa e o sol"
