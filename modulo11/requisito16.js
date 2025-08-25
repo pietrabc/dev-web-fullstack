c35cd49925ef2befe2695bffbbf68157da60b50c
pietrabc <pietrabezerracardoso@gmail.com>
2025-08-25 17:15:09 -0300

    hashtag-js

--- modulo11/requisito16.js
+++ modulo11/requisito16.js
@@ -11,62 +11,90 @@
 
 const fraseCodificar = 'a casa e o Sol sem miudos';
 const resultado = codificar(fraseCodificar);
-const fraseDecodificar = '1 c1s2 2 4 s4l s2m m35d4s';
+const fraseDecodificar = '1 c1s1 2 4 s4l s2m m35d4s';
 //const resultado2 = decodificar(fraseDecodificar);
 
 function codificar(frase) {
-   let resultado = frase;
+  let resultado = frase;
 
-   //resultado = resultado.replace('a','1');
-   while(resultado.includes('a')) {
-    resultado = resultado.replace('a', '1')
-   } 
-   while(resultado.includes('e')) {
-    resultado = resultado.replace('e', '2')
-   } 
+  //resultado = resultado.replace('a','1');
+  while (resultado.includes('a')) {
+    resultado = resultado.replace('a', '1');
+  }
+  while (resultado.includes('e')) {
+    resultado = resultado.replace('e', '2');
+  }
 
-   while(resultado.includes('i')) {
-    resultado = resultado.replace('i', '3')
-   } 
+  while (resultado.includes('i')) {
+    resultado = resultado.replace('i', '3');
+  }
 
-   while(resultado.includes('o')) {
-    resultado = resultado.replace('o', '4')
-   } 
+  while (resultado.includes('o')) {
+    resultado = resultado.replace('o', '4');
+  }
 
-   while(resultado.includes('u')) {
-    resultado = resultado.replace('u', '5')
-   } 
+  while (resultado.includes('u')) {
+    resultado = resultado.replace('u', '5');
+  }
 
-   return resultado;
-
+  return resultado;
 }
-
-//console.log(codificar(fraseCodificar))
 
 function decodificar(frase) {
-   let resultado = frase;
+  let resultado = frase;
 
-   while(resultado.includes('1')) {
-      resultado = resultado.replace('1', 'a')
-   }
+  while (resultado.includes('1')) {
+    resultado = resultado.replace('1', 'a');
+  }
 
-   while(resultado.includes('2')) {
-      resultado = resultado.replace('2', 'e')
-   }
+  while (resultado.includes('2')) {
+    resultado = resultado.replace('2', 'e');
+  }
 
-   while(resultado.includes('3')) {
-      resultado = resultado.replace('3', 'i')
-   }
+  while (resultado.includes('3')) {
+    resultado = resultado.replace('3', 'i');
+  }
 
-   while(resultado.includes('4')) {
-      resultado = resultado.replace('4', 'o')
-   }
+  while (resultado.includes('4')) {
+    resultado = resultado.replace('4', 'o');
+  }
 
-   while(resultado.includes('5')) {
-      resultado = resultado.replace('5', 'u')
-   }
+  while (resultado.includes('5')) {
+    resultado = resultado.replace('5', 'u');
+  }
 
-   return resultado;
+  return resultado;
 }
 
-console.log(decodificar(fraseDecodificar));
+const fraseParaDecodificar = codificar(fraseCodificar);
+
+//console.log(codificar(fraseCodificar))
+//console.log(decodificar(fraseDecodificar));
+
+// Expressões regulares - Regex [a-z] [A-Z]
+
+// Refaturação 25 linhas código - 10 linhas
+
+function codificarRegex(frase) {
+  let resultado = frase
+    .replace(/a/g, '1')
+    .replace(/e/g, '2')
+    .replace(/i/g, '3')
+    .replace(/o/g, '4')
+    .replace(/u/g, '5');
+
+  return resultado;
+}
+
+function decodificarRegex(frase) {
+  let resultado = frase
+    .replace(/1/g, 'a')
+    .replace(/2/g, 'e')
+    .replace(/3/g, 'i')
+    .replace(/4/g, 'o')
+    .replace(/5/g, 'u');
+
+  return resultado;
+}
+
+console.log(decodificarRegex(fraseDecodificar));
---
+++ modulo11/simulacao.js
@@ -0,0 +1,50 @@
+// Requisitos do Projeto de Simulação Financeira
+// Lembre-se de que os requisitos podem ser resolvidos com diferentes abordagens, e nosso
+// gabarito é apenas uma forma de implementação.
+
+// Requisito 1: Simulação de Investimento
+// ❖ Crie uma função que simule o rendimento de um investimento com base em um valor
+// inicial, uma taxa mensal de rendimento e um período de meses.
+// ❖ A função deve retornar o saldo final após o período especificado, com duas casas
+// decimais.
+// Função: simularInvestimento
+
+// Requisito 2: Gerenciamento de Despesas
+// ❖ Crie uma função que receba um objeto representando as despesas mensais em
+// diferentes categorias.
+// ❖ A função deve calcular e retornar o total de despesas.
+// Função: gerenciarDespesas
+
+// Requisito 3: Obtenção do Mês Atual
+// ❖ Crie uma função que retorne o nome do mês atual em formato string.
+// ❖ A função deve utilizar um array de nomes dos meses para identificar o mês atual.
+// Função: obterMesAtual
+
+// Requisito 4: Geração de Relatório Financeiro
+// ❖ Crie uma função que gere um relatório financeiro com base no investimento inicial, taxa
+// de rendimento, duração do investimento, despesas mensais, meta de investimento e
+// meta de orçamento.
+// ❖ O relatório deve incluir:
+// ■ O mês atual.
+// ■ O saldo final do investimento.
+// ■ O total de despesas.
+// ■ Uma mensagem de economia ou excedente em relação ao orçamento.
+// ■ Uma verificação se a meta de investimento foi atingida.
+// ■ Um resumo das despesas por categoria.
+// Função: gerarRelatorio
+
+// Requisito 5: Estrutura de Dados para Despesas
+// ❖ Crie um objeto para armazenar as despesas mensais, utilizando categorias como
+// alimentação, transporte, aluguel e lazer.
+// ❖ O objeto deve permitir fácil acesso e modificação dos valores das despesas.
+
+// Requisito 6: Entradas e Configurações do Investimento
+// ❖ Defina variáveis para armazenar o valor inicial do investimento, a taxa mensal de
+// rendimento, a duração do investimento em meses, a meta de investimento e a meta de
+// orçamento mensal.
+// ❖ As variáveis devem ser configuráveis para permitir diferentes simulações financeiras.
+
+// Requisito 7: Execução do Relatório
+// ❖ Chame a função gerarRelatorio com os parâmetros adequados para gerar e exibir
+// o relatório financeiro no console.
+// ❖ A chamada deve utilizar as variáveis e objetos definidos anteriormente
