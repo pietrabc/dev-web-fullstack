9a09bcf5c3964a9f48accb42b0280e14c4c54fce
pietrabc <pietrabezerracardoso@gmail.com>
2025-09-05 18:32:24 -0300

    hashtag-html

---
+++ modulo12/mod01.js
@@ -0,0 +1,11 @@
+function onClick() {
+    console.log('Clicando no botão')
+}
+
+function onMouseOver() {
+    console.log('Passando por cima')
+}
+
+function onKeyPress()  {
+    console.log('Digitando no input')
+}
